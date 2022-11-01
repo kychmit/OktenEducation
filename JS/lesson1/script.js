675a2e740b17d397652124f3699d3e10d3341c6f
kychmit <katrustim@gmail.com>
2022-11-01 14:19:36 +0300

    update

---
+++ JS/lesson1/script.js
@@ -0,0 +1,51 @@
+let a = 'Hello'
+let b = 'Owu'
+let c = '.com'
+let d = '.ua'
+console.log(a + b + c + d)
+
+let e = 1
+console.log(e)
+
+let f = 10
+console.log(f)
+
+let g = -999
+console.log(g)
+
+let h = 123
+console.log(h)
+
+const PI = 3.14
+console.log(PI)
+
+let s = 2.7
+console.log(s)
+
+console.log(9 + 7)
+
+let j = true
+console.log(j)
+
+let k = false
+console.log(k)
+
+let firstName = 'Kateryna '
+let middlename = 'Ruslanivna '
+let lastname = 'Tymchuk'
+let persone = firstName + middlename + lastname
+console.log( persone)
+
+let x = 100
+console.log(typeof (x))
+
+let m = '100'
+console.log(typeof (m))
+
+let o = true
+console.log(typeof (o))
+
+let name = prompt ( 'Hello, what is your  First name ?');
+let last = prompt ( 'What is your Last name ?');
+let age = prompt ( 'How old are you ?');
+console.log( name + ' ' +  last + ' ' +  age )
